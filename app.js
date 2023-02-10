930d0bdb5210ae5be5482a9b9472be19ebbba4b8
MOHAMED MAHAMOUD NAGUIB <naguib.mohamed-mahamoud@etu.univ-lyon1.fr>
2023-02-10 18:02:08 +0100

    added cors

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-
+const cors = require('cors');
+app.use(cors());
 app.set('view engine', 'ejs');
 
 const port = process.env.PORT || 3000;
 require('./models/dbConfig');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 
 const getsRouter = require('./routes/getsController');
 const postsRouter = require('./routes/postsController');
